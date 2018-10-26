167e7ab66e4ead106169f1432b975d9436f44f39
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-10-26 14:27:16 +0300

    меню

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,12 +14,12 @@
     isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
     windowReady = false,
     isNoviBuilder = false,
-    livedemo = false,
 
     plugins = {
       rdNavbar: $(".rd-navbar"),
       wow: $(".wow"),
       multitoggle: document.querySelectorAll( '[data-multitoggle]' ),
+      pageLoader: $(".page-loader"),
     };
   $window.on('load', function () {
     // RD Navbar
@@ -30,70 +30,83 @@
       values = [0, 576, 768, 992, 1200, 1600];
       responsiveNavbar = {};
 
-      for (i = j = 0, len = values.length; j < len; i = ++j) {
-        value = values[i];
-        if (!responsiveNavbar[values[i]]) {
-          responsiveNavbar[values[i]] = {};
-        }
-        if (plugins.rdNavbar.attr('data' + aliaces[i] + 'layout')) {
-          responsiveNavbar[values[i]].layout = plugins.rdNavbar.attr('data' + aliaces[i] + 'layout');
-        }
-        if (plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout')) {
-          responsiveNavbar[values[i]]['deviceLayout'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout');
-        }
-        if (plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on')) {
-          responsiveNavbar[values[i]]['focusOnHover'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on') === 'true';
-        }
-        if (plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height')) {
-          responsiveNavbar[values[i]]['autoHeight'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height') === 'true';
-        }
+      for (var z = 0; z < plugins.rdNavbar.length; z++) {
+        var $rdNavbar = $(plugins.rdNavbar[z]);
 
-        if (isNoviBuilder) {
-          responsiveNavbar[values[i]]['stickUp'] = false;
-        } else if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up')) {
-          responsiveNavbar[values[i]]['stickUp'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up') === 'true';
-        }
+        for (i = j = 0, len = values.length; j < len; i = ++j) {
+          value = values[i];
+          if (!responsiveNavbar[values[i]]) {
+            responsiveNavbar[values[i]] = {};
+          }
+          if ($rdNavbar.attr('data' + aliaces[i] + 'layout')) {
+            responsiveNavbar[values[i]].layout = $rdNavbar.attr('data' + aliaces[i] + 'layout');
+          }
+          if ($rdNavbar.attr('data' + aliaces[i] + 'device-layout')) {
+            responsiveNavbar[values[i]]['deviceLayout'] = $rdNavbar.attr('data' + aliaces[i] + 'device-layout');
+          }
+          if ($rdNavbar.attr('data' + aliaces[i] + 'hover-on')) {
+            responsiveNavbar[values[i]]['focusOnHover'] = $rdNavbar.attr('data' + aliaces[i] + 'hover-on') === 'true';
+          }
+          if ($rdNavbar.attr('data' + aliaces[i] + 'auto-height')) {
+            responsiveNavbar[values[i]]['autoHeight'] = $rdNavbar.attr('data' + aliaces[i] + 'auto-height') === 'true';
+          }
 
-        if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset')) {
-          responsiveNavbar[values[i]]['stickUpOffset'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset');
-        }
-      }
+          if (isNoviBuilder) {
+            responsiveNavbar[values[i]]['stickUp'] = false;
+          } else if ($rdNavbar.attr('data' + aliaces[i] + 'stick-up')) {
+            var isDemoNavbar = $rdNavbar.parents('.layout-navbar-demo').length;
+            responsiveNavbar[values[i]]['stickUp'] = $rdNavbar.attr('data' + aliaces[i] + 'stick-up') === 'true' && !isDemoNavbar;
+          }
 
-
-      plugins.rdNavbar.RDNavbar({
-        anchorNav: !isNoviBuilder,
-        stickUpClone: (plugins.rdNavbar.attr("data-stick-up-clone") && !isNoviBuilder) ? plugins.rdNavbar.attr("data-stick-up-clone") === 'true' : false,
-        responsive: responsiveNavbar,
-        callbacks: {
-          onStuck: function () {
-            var navbarSearch = this.$element.find('.rd-search input');
-
-            if (navbarSearch) {
-              navbarSearch.val('').trigger('propertychange');
-            }
-          },
-          onDropdownOver: function () {
-            return !isNoviBuilder;
-          },
-          onUnstuck: function () {
-            if (this.$clone === null)
-              return;
-
-            var navbarSearch = this.$clone.find('.rd-search input');
-
-            if (navbarSearch) {
-              navbarSearch.val('').trigger('propertychange');
-              navbarSearch.trigger('blur');
-            }
-
+          if ($rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset')) {
+            responsiveNavbar[values[i]]['stickUpOffset'] = $rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset');
           }
         }
-      });
+
+        $rdNavbar.RDNavbar({
+          anchorNav: !isNoviBuilder,
+          stickUpClone: ($rdNavbar.attr("data-stick-up-clone") && !isNoviBuilder) ? $rdNavbar.attr("data-stick-up-clone") === 'true' : false,
+          responsive: responsiveNavbar,
+          callbacks: {
+            onStuck: function () {
+              var navbarSearch = this.$element.find('.rd-search input');
+
+              if (navbarSearch) {
+                navbarSearch.val('').trigger('propertychange');
+              }
+            },
+            onDropdownOver: function () {
+              return !isNoviBuilder;
+            },
+            onUnstuck: function () {
+              if (this.$clone === null)
+                return;
+
+              var navbarSearch = this.$clone.find('.rd-search input');
+
+              if (navbarSearch) {
+                navbarSearch.val('').trigger('propertychange');
+                navbarSearch.trigger('blur');
+              }
+
+            }
+          }
+        });
 
 
-      if (plugins.rdNavbar.attr("data-body-class")) {
-        document.body.className += ' ' + plugins.rdNavbar.attr("data-body-class");
+        if ($rdNavbar.attr("data-body-class")) {
+          document.body.className += ' ' + $rdNavbar.attr("data-body-class");
+        }
+
       }
+    }
+
+    //Page loader
+    if (plugins.pageLoader.length && !isNoviBuilder) {
+        var loader = setTimeout(function () {
+          plugins.pageLoader.addClass("loaded");
+          $window.trigger("resize");
+        }, 200);
     }
 
     // Multitoggles
@@ -103,4 +116,10 @@
 
   });
 
+  $(function () {
+    isNoviBuilder = window.xMode;
+  });
+
 }());
+
+
