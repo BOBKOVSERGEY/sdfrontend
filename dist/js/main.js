7a464d0edf370b48d67cf82d05d0927df4b8c5c2
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-02-07 16:51:42 +0300

    start form

--- dist/js/main.js
+++ dist/js/main.js
@@ -341,6 +341,9 @@ if (document.querySelector('#loader-wrapper')) {
     $('.js-policy-5').on('click', function(){
       checkPolicy(this, '.form__btn-5');
     });
+    $('.js-policy-6').on('click', function(){
+      checkPolicy(this, '.form__btn-6');
+    });
 
 
 
@@ -1666,6 +1669,33 @@ if (document.querySelector('#loader-wrapper')) {
     }
 
   });
+
+
+
+  /*******inputfile******/
+
+  var inputs = document.querySelectorAll( '.inputfile' );
+  Array.prototype.forEach.call( inputs, function( input )
+  {
+    var label	 = input.nextElementSibling,
+      labelVal = label.innerHTML;
+
+    input.addEventListener( 'change', function( e )
+    {
+      var fileName = '';
+      if( this.files && this.files.length > 1 )
+        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+      else
+        fileName = e.target.value.split( '\\' ).pop();
+
+      if( fileName )
+        label.querySelector( 'span' ).innerHTML = fileName;
+      else
+        label.innerHTML = labelVal;
+    });
+  });
+
+  /*******end inputfile******/
 
 
 }());
