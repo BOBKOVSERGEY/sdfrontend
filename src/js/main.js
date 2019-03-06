7905a14b6039d22fe672e649fcdbcfcfd55ff48d
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-03-06 17:06:00 +0300

    Popup form

--- src/js/main.js
+++ src/js/main.js
@@ -139,6 +139,63 @@ if (document.querySelector('#loader-wrapper')) {
       }
     });
 
+    /*validate form-popup*/
+    $('.form-popup').validate({
+      /*submitHandler: function(form){
+        //var form = document.forms.formContact,
+        var formData = new FormData(form),
+          xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "/send-main.php");
+
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4) {
+            if(xhr.status == 200) {
+
+              var contactLoader = document.querySelector('.contact-form__loader-wrapper');
+              contactLoader.style.visibility = 'visible';
+              contactLoader.style.opacity = '1';
+              $('.form-contact')[0].reset();
+              $('.upload-text-reset').text('Прикрепить файл');
+              setTimeout(function () {
+                contactLoader.style.visibility = 'hidden';
+                contactLoader.style.opacity = '0';
+              }, 3000);
+              //$(".modal-popup__answer").html('<div class="modal-popup__form-tanks">Заявка успешно отправлена!<div>');
+            } else {
+              //$(".modal-popup__answer").html('<div class="modal-popup__form-tanks">Что то пошло не так!<div>');
+            }
+          }
+        };
+        xhr.send(formData);
+      },*/
+      rules: {
+        name: {
+          required: true,
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        phone: {
+          required: true
+        }
+
+      },
+      messages: {
+        name: {
+          required: "Введите Ваше имя"
+        },
+        email: {
+          required: "Введите адрес электронной почты",
+          email: "Не корректный адрес"
+        },
+        phone: {
+          required: "Введите Ваш телефон"
+        }
+      }
+    });
+
     /*validate form-nav*/
     $('.form-nav').validate({
       submitHandler: function(form){
